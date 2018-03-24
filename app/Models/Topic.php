f205050c035538716fa50a09900c4664d5336420
mushishixian <648132694@qq.com>
2018-03-24 03:21:41 +0000

    User can create topic

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -4,18 +4,7 @@ namespace App\Models;
 
 class Topic extends Model
 {
-    protected $fillable = [
-        'title',
-        'body',
-        'user_id',
-        'category_id',
-        'reply_count',
-        'view_count',
-        'last_reply_user_id',
-        'order',
-        'excerpt',
-        'slug',
-    ];
+    protected $fillable = ['title', 'body', 'category_id', 'excerpt', 'slug'];
 
     public function scopeWithOrder($query, $order)
     {
@@ -29,6 +18,7 @@ class Topic extends Model
                 $query = $this->recentReplied();
                 break;
         }
+
         // 预加载防止 N+1 问题
         return $query->with('user', 'category');
     }
