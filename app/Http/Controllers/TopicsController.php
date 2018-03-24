7ddca109b6472814a6a7d7f646b181e42ccc7e2b
mushishixian <648132694@qq.com>
2018-03-24 06:05:23 +0000

    Topic detail page

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -42,7 +42,7 @@ class TopicsController extends Controller
         $topic->user_id = \Auth::id();
         $topic->save();
 
-        return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+        return redirect()->route('topics.show', $topic->id)->with('success', 'Created successfully.');
     }
 
     public function edit(Topic $topic)
@@ -57,7 +57,7 @@ class TopicsController extends Controller
         $this->authorize('update', $topic);
         $topic->update($request->all());
 
-        return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+        return redirect()->route('topics.show', $topic->id)->with('success', 'Updated successfully.');
     }
 
     public function destroy(Topic $topic)
@@ -65,7 +65,7 @@ class TopicsController extends Controller
         $this->authorize('destroy', $topic);
         $topic->delete();
 
-        return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+        return redirect()->route('topics.index')->with('success', 'Deleted successfully.');
     }
 
     public function uploadImage(Request $request, ImageUploadHandler $uploader)
