2005c823305dcf8a4e06097aeb32ab2261b3a391
mushishixian <648132694@qq.com>
2018-03-24 00:30:02 +0000

    Generated topics

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,5 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 Route::get('/', 'PagesController@root')->name('root');
 
 Route::resource('/users','UsersController', ['only' => ['show', 'update', 'edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
