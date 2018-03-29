b0657c650134f474dfc53a7dc910c7dc46c7846d
mushishixian <648132694@qq.com>
2018-03-29 02:40:12 +0000

    api/user

--- routes/api.php
+++ routes/api.php
@@ -19,19 +19,29 @@ $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api',
     'middleware' => 'serializer:array|cors',
 ], function ($api) {
-    //游客可以访问的接口
-    $api->get('categories', 'CategoriesController@index')
-        ->name('api.categories.index');
-
-    $api->get('topics', 'TopicsController@index');
-
-    // 登录
-    $api->post('authorizations', 'AuthorizationsController@store')
-        ->name('api.authorizations.store');
-    $api->put('authorizations/current', 'AuthorizationsController@update')
-        ->name('api.authorizations.update');
-    $api->delete('authorizations/current', 'AuthorizationsController@destroy')
-        ->name('api.authorizations.destroy');
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.access.limit'),
+        'expires' => config('api.rate_limits.access.expires'),
+    ], function ($api) {
+        //游客可以访问的接口
+        $api->get('categories', 'CategoriesController@index')
+            ->name('api.categories.index');
+        $api->get('topics', 'TopicsController@index');
+        // 登录
+        $api->post('authorizations', 'AuthorizationsController@store')
+            ->name('api.authorizations.store');
+        $api->put('authorizations/current', 'AuthorizationsController@update')
+            ->name('api.authorizations.update');
+        $api->delete('authorizations/current', 'AuthorizationsController@destroy')
+            ->name('api.authorizations.destroy');
+        // 需要 token 验证的接口
+        $api->group(['middleware' => 'api.auth'], function($api) {
+            // 当前登录用户信息
+            $api->get('user', 'UsersController@me')
+                ->name('api.user.show');
+        });
+    });
 
     $api->group([
         'middleware' => 'api.throttle',
